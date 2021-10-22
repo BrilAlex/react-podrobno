d7bc526c22261baca8a9dca8049b2148cd4a0d46
BrilAlex <brilalex.it@yandex.ru>
2021-10-22 22:23:18 +0300

    Lesson 21 useEffect - setTimeout, setInterval done

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -37,3 +37,53 @@ export const UseEffectExample = () => {
         </>
     );
 }
+
+export const UseEffectSetTimeoutAndSetIntervalExample = () => {
+    console.log("UseEffect SetTimeout And SetInterval Example rendered");
+    const [fake, setFake] = useState(0);
+    const [counter, setCounter] = useState(0);
+    const [timer, setTimer] = useState(0);
+
+    useEffect(() => {
+        console.log("useEffect setTimeout called");
+        setTimeout(() => {
+            console.log("setTimeout");
+            document.title = counter.toString();
+        }, 1000);
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("useEffect setInterval called");
+        setInterval(() => {
+            console.log("setInterval " + timer);
+            setTimer(state => state + 1);
+        }, 1000);
+    }, []);
+
+    return (
+        <>
+            Timer: {timer}<br/>
+            Hello, counter: {counter} and fake: {fake}<br/>
+            <button onClick={() => setCounter(counter + 1)}>Counter +</button>
+            <button onClick={() => setFake(fake + 1)}>Fake +</button><br/>
+        </>
+    );
+}
+
+export const UseEffectTimeExample = () => {
+    const [date, setDate] = useState<Date>();
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+    }, []);
+
+    let time = date?.toLocaleTimeString();
+
+    return (
+        <>
+            Current time: {time}
+        </>
+    );
+}
